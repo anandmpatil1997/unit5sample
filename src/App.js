19d056f589e6af725be6d3c969df474123d6a176
anandmpatil1997 <96177274+anandmpatil1997@users.noreply.github.com>
2022-04-08 16:47:00 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
 import "./styles.css";
 
 export default function App() {
-  return <div className="App"></div>;
+  const mobiles = ["Android", "Blackbery", "iphone", "windows phone"];
+  const manufac = ["samsung", "HTC", "micromax", "apple"];
+  return (
+    <div className="App">
+      <h1> Mobile Operating System</h1>
+      {mobiles.map((e) => {
+        return (
+          <ul>
+            <li>{e}</li>
+          </ul>
+        );
+      })}
+      <div>
+        <h1> Mobile Manufacturers</h1>
+        {manufac.map((e) => {
+          return (
+            <ul>
+              <li>{e}</li>
+            </ul>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
